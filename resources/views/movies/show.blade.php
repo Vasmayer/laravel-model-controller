1f900bb9b9136cbf37cc8023d898ee8bb3777e0d
Vasmayer <lealimarco90@gmail.com>
2022-03-17 18:26:54 +0100

    es completed

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <h1 class="text-center">MOVIE</h1>
     <div class="container d-flex flex-wrap">
-      <div class="card w-50 text-center">
+      <div class="card w-50 text-center m-auto">
         <div class="card-body">
           @include('includes.card')
         </div>
